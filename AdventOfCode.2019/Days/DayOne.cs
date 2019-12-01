fe87bf55b330a043d0ae0f2e75e3bef8d8190e48
James M Anderson <james@gaidge.com>
2019-12-01 01:19:28 -0500

    2019 Day One - Complete

--- AdventOfCode.2019/Days/DayOne.cs
+++ AdventOfCode.2019/Days/DayOne.cs
@@ -8,8 +8,35 @@ namespace AdventOfCode._2019.Days
         public static Day GetDay()
         {
             var day = new Day();
-                        
+            
+            var masses = System.IO.File.ReadAllText("Input/DayOne.txt").Split("\r\n");
+
+            var fuelForRawMass = 0;
+            var totalFuel = 0;
+
+            foreach( var massString in masses)
+            {
+                var mass = int.Parse(massString);
+
+                fuelForRawMass += mass / 3 - 2;
+                totalFuel += GetFuelForMass(mass);
+            }
+
+            day.PartOne.Result = fuelForRawMass.ToString();
+            day.PartTwo.Result = totalFuel.ToString();
             return day;
+        }
+
+        private static int GetFuelForMass(int mass)
+        {
+            var fuel = mass / 3 - 2;
+
+            if(fuel < 0)
+            {
+                return 0;
+            }
+
+            return fuel + GetFuelForMass(fuel);
         }
     }    
 }
