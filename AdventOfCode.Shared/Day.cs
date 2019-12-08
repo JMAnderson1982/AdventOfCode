8e07bf6cdcc78b7d64be5e6beb27aa0ff1610fd3
James M Anderson <james@gaidge.com>
2019-12-08 10:28:30 -0500

    Day 8 done

--- AdventOfCode.Shared/Day.cs
+++ AdventOfCode.Shared/Day.cs
@@ -37,7 +37,8 @@ namespace AdventOfCode.Shared
                 using( var client = new HttpClient())
                 {
                     client.DefaultRequestHeaders.Add("Cookie", "session=53616c7465645f5fcc1dfcc61fccbb4ade57925b3c7018f89453d251b4de6d15de001b8bbe47daa2ff5867576bb937ec");
-                    Input = await client.GetStringAsync($"https://adventofcode.com/2019/day/{Date}/input");
+                    Input = (await client.GetStringAsync($"https://adventofcode.com/2019/day/{Date}/input")).Trim();
+                    
                 }
                 return;
             }
