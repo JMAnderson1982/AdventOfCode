bcca5bf28c51bd14ccca39f13c610d5645986ba8
James M Anderson <james@gaidge.com>
2020-12-12 00:44:26 -0500

    Bargle

---
+++ AdventOfCode.2020/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AdventOfCode._2020
+{
+    using System;
+    using AdventOfCode.Shared;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var advent = new Advent{ Year = 2019 };
+
+            advent.ShowResults();
+        }
+    }
+}
